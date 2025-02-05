c4497e310416c5a8bcb4d21137a6da54e9c026a5
Tiago Ferreira <ferreiratiago@outlook.com>
2025-02-05 22:18:36 +0000

    test/20240205_2218

--- sql_injection.js
+++ sql_injection.js
@@ -1,8 +1,9 @@
 function checkLogin(req, db) {
+  const password_token = 'super-secret-f8ed84e8f41e4146403dd4a6bbcea5e418d24i7';
   const sqlQuery =
     "SELECT email FROM credentials WHERE " +
-    "email = ? AND " +
-    "password = ?";
+    "email = '" + req.body.email + "' AND " +
+    "password = '" + req.body.password + "'";
 
     doQuery(sqlQuery, [])
 }
